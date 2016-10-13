e81a194ff9960df7ceb329f9fba7379e7d3cdab7
Jen <hanratty.jen@gmail.com>
2016-10-13 13:47:35 +0100

    Update confirmation_text_sender.rb

--- lib/confirmation_text_sender.rb
+++ lib/confirmation_text_sender.rb
@@ -6,13 +6,13 @@ class ConfirmationTextSender
 
   def send_message(message)
 
-    account_sid = 'AC9418dc2fc0bb5cbb33e505c227d3a3c1'
-    auth_token = '5490266efdf7640c6e1d59a7da563967'
+    account_sid = 'xx'
+    auth_token = 'xxx'
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     @message = @client.messages.create(
-      to: "+447984599636",
-      from: "+441173253687",
+      to: "+xx",
+      from: "+xx",
       body: "Hi from FOOD! Your order has cost £#{message} and will arrive at #{calculate_time}."
     )
   end
